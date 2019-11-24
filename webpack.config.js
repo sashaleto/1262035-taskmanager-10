51fa766905d4c15c11cc5f399e6c85da65e1e9c2
sashaleto <aleks.nesc@gmail.com>
2019-11-24 16:29:25 +0300

    В проект добавлен Webpack Dev Server, изменен путь до bundle.js
    (cherry picked from commit 6b085a492026d99864119969a7c341f2be0aeb63)

--- webpack.config.js
+++ webpack.config.js
@@ -8,4 +8,10 @@ module.exports = {
     filename: `bundle.js`,
   },
   devtool: `source-map`,
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    publicPath: 'http://localhost:8080',
+    compress: true,
+    watchContentBase: true,
+  },
 };
