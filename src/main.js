4693c66a0a19ed4aca7150b728dccd042475fb40
sashaleto <aleks.nesc@gmail.com>
2019-12-09 00:03:26 +0300

    Отображение сгенерированных данных на странице

--- src/main.js
+++ src/main.js
@@ -4,8 +4,14 @@ import {createTaskListTemplate} from './components/board.js';
 import {createSingleTaskTemplate} from './components/task-card.js';
 import {createTaskEditFormTemplate} from './components/task-card--edit.js';
 import {createLoadMoreBtnTemplate} from './components/load-more-button.js';
+import {generateTasks} from './mocks/task.js';
+import {generateFilters} from './mocks/filters.js';
+import {FILTER_TITLES} from './constants.js';
 
-const TASK_COLORS = [`black`, `blue`, `yellow`]; // цвета карточек
+const TASK_COUNT = 22;
+
+const tasks = generateTasks(TASK_COUNT);
+const filters = generateFilters(FILTER_TITLES);
 
 const mainElement = document.querySelector(`.main`);
 const menuContainer = mainElement.querySelector(`.main__control`);
@@ -17,13 +23,13 @@ const renderComponent = (container, markup, position) => {
 };
 
 renderComponent(menuContainer, createMenuTemplate(), `beforeend`);
-renderComponent(mainElement, createFiltersTemplate(), `beforeend`);
+renderComponent(mainElement, createFiltersTemplate(filters), `beforeend`);
 renderComponent(mainElement, createTaskListTemplate(), `beforeend`);
 
 boardElement = mainElement.querySelector(`.board__tasks`);
 
-renderComponent(boardElement, createTaskEditFormTemplate(), `beforeend`);
+renderComponent(boardElement, createTaskEditFormTemplate(tasks[0]), `beforeend`);
 
-TASK_COLORS.forEach((color) => renderComponent(boardElement, createSingleTaskTemplate(color), `beforeend`));
+tasks.slice(1).forEach((task) => renderComponent(boardElement, createSingleTaskTemplate(task), `beforeend`));
 
 renderComponent(boardElement, createLoadMoreBtnTemplate(), `afterend`);
