375b16510ca05065f9b438dc5acddfd3f6123099
sashaleto <aleks.nesc@gmail.com>
2019-12-08 23:48:45 +0300

    Добавление констант и вспомогательных функций

---
+++ src/constants.js
@@ -0,0 +1,20 @@
+export const MONTH_NAMES = [
+  `January`,
+  `February`,
+  `March`,
+  `April`,
+  `May`,
+  `June`,
+  `July`,
+  `August`,
+  `September`,
+  `October`,
+  `November`,
+  `December`,
+];
+
+export const FILTER_TITLES = [`All`, `Overdue`, `Today`, `Favorites`, `Repeating`, `Tags`, `Archive`];
+
+export const DAYS = [`mo`, `tu`, `we`, `th`, `fr`, `sa`, `su`];
+
+export const COLORS = [`black`, `yellow`, `blue`, `green`, `pink`];
---
+++ src/utils.js
@@ -0,0 +1,30 @@
+/**
+ * Генерация случайного числа в диапазоне от 0 до max
+ * @param {number} max - максимальное значение
+ * @return {number} - рандомное число
+ */
+export const getRandomNumber = (max) => {
+  return Math.floor(Math.random() * Math.floor(max));
+};
+
+export const getRandomArrayItem = (array) => {
+  return array[getRandomNumber(array.length)];
+};
+
+const castTimeFormat = (value) => {
+  return value < 10 ? `0${value}` : String(value);
+};
+
+/**
+ * Приведение формата времени в вид: "hh:mm a"
+ * @param {Date} date - исходная дата-время
+ * @return {string} - отформатированное время
+ */
+export const formatTime = (date) => {
+  const hours = castTimeFormat(date.getHours() % 12);
+  const minutes = castTimeFormat(date.getMinutes());
+
+  const interval = date.getHours() > 11 ? `PM` : `AM`;
+
+  return `${hours}:${minutes} ${interval}`;
+};
