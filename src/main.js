75c7ca119cc118c068fa30d5feb02092ae40d21a
sashaleto <aleks.nesc@gmail.com>
2020-01-12 17:44:58 +0300

    Кнопка load more переписана в класс; изменен рендер кнопки load more

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import FilterComponent from './components/main-filter.js';
 import TaskListComponent from './components/board.js';
 import TaskComponent from './components/task-card.js';
 import TaskEditFormComponent from './components/task-card--edit.js';
-import {createLoadMoreBtnTemplate} from './components/load-more-button.js';
+import LoadMoreComponent from './components/load-more-button.js';
 import {generateTasks} from './mocks/task.js';
 import {generateFilters} from './mocks/filters.js';
 import {FILTER_TITLES, RenderPosition} from './constants.js';
@@ -18,30 +18,23 @@ const filters = generateFilters(FILTER_TITLES);
 
 const mainElement = document.querySelector(`.main`);
 const menuContainer = mainElement.querySelector(`.main__control`);
-let boardElement;
-
-// Функция для рендеринга компонентов
-const renderComponent = (container, markup, position) => {
-  container.insertAdjacentHTML(position, markup);
-};
 
 render(menuContainer, new MainMenuComponent().getElement(), RenderPosition.BEFOREEND);
-
 render(mainElement, new FilterComponent(filters).getElement(), RenderPosition.BEFOREEND);
 
 const taskList = new TaskListComponent();
 render(mainElement, taskList.getElement(), RenderPosition.BEFOREEND);
 
-boardElement = mainElement.querySelector(`.board__tasks`);
-
+const boardElement = mainElement.querySelector(`.board__tasks`);
 render(boardElement, new TaskEditFormComponent(tasks[0]).getElement(), RenderPosition.BEFOREEND);
 
 let lastShownCardNumber = INITIALLY_SHOWN_TASKS_COUNT;
 tasks.slice(1, lastShownCardNumber).forEach((task) => render(boardElement, new TaskComponent(task).getElement(), RenderPosition.BEFOREEND));
 
-renderComponent(boardElement, createLoadMoreBtnTemplate(), `afterend`);
+const loadMoreComponent = new LoadMoreComponent();
+const loadMoreButton = loadMoreComponent.getElement();
 
-const loadMoreButton = mainElement.querySelector(`.load-more`);
+render(taskList.getElement(), loadMoreButton, RenderPosition.BEFOREEND);
 
 loadMoreButton.addEventListener(`click`, () => {
   const increasedCardNumber = lastShownCardNumber + NEXT_SHOWN_TASKS_COUNT;
@@ -50,6 +43,7 @@ loadMoreButton.addEventListener(`click`, () => {
   lastShownCardNumber = increasedCardNumber;
 
   if (increasedCardNumber >= tasks.length) {
+    loadMoreComponent.removeElement();
     loadMoreButton.remove();
   }
 });
