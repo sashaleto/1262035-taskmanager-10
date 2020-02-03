7795af9963da643a7ceb53c2e817d0aa7feb36fd
sashaleto <aleks.nesc@gmail.com>
2020-02-03 13:04:27 +0300

    Добавлены методы сбора данных из формы

--- src/components/task-card--edit.js
+++ src/components/task-card--edit.js
@@ -180,6 +180,25 @@ const createTaskEditFormTemplate = (task, options = {}) => {
   `;
 };
 
+const parseFormData = (formData) => {
+  const repeatingDays = DAYS.reduce((acc, day) => {
+    acc[day] = false;
+    return acc;
+  }, {});
+  const date = formData.get(`date`);
+
+  return {
+    description: formData.get(`text`),
+    color: formData.get(`color`),
+    tags: formData.getAll(`hashtag`),
+    dueDate: date ? new Date(date) : null,
+    repeatingDays: formData.getAll(`repeat`).reduce((acc, it) => {
+      acc[it] = true;
+      return acc;
+    }, repeatingDays),
+  };
+};
+
 export default class TaskEditFormComponent extends AbstractSmartComponent {
   constructor(task) {
     super();
@@ -193,6 +212,13 @@ export default class TaskEditFormComponent extends AbstractSmartComponent {
 
     this._applyFlatpickr();
     this._subscribeOnEvents();
+  }
+
+  getData() {
+    const form = this.getElement().querySelector(`.card__form`);
+    const formData = new FormData(form);
+
+    return parseFormData(formData);
   }
 
   getTemplate() {
